7a44efca9dfe2aa080c3be75466e1f63adc9d335
jamie wright <https://jamieianwright@bitbucket.org/jamieianwright/>
2019-03-01 23:07:18 +0000

    Experience now populated dynamically  by wp rather than static HTML

--- wp-content/themes/JWT_portfolio_website_theme/partials/experience.php
+++ wp-content/themes/JWT_portfolio_website_theme/partials/experience.php
@@ -2,38 +2,36 @@
       <div class="w-100">
         <h2 class="mb-5">Experience</h2>
 
-        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-          <div class="resume-content">
-            <h3 class="mb-0">Key Stage 3 Coordinator, Teacher of Science</h3>
-            <div class="subheading mb-3">Northampton School for Girls</div>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-          </div>
-          <div class="resume-date text-md-right">
-            <span class="text-primary">January 2018 - Present</span>
-          </div>
-        </div>
+        <?php 
+          $jobs = new WP_Query(array(
+            'post_type' => 'Experience',
+            'orderby' => 'start_date',
+            'order' => 'ASC',
+          ));
 
-        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-          <div class="resume-content">
-            <h3 class="mb-0">Key Stage 4 Coordinator, Teacher of Science</h3>
-            <div class="subheading mb-3">Danetree and Southbrook Learning Village</div>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-          </div>
-          <div class="resume-date text-md-right">
-            <span class="text-primary">September 2016 - December 2017</span>
-          </div>
-        </div>
+          while($jobs->have_posts()) {
+            $jobs->the_post();
+            $formattedEndDate = 'Present';
 
-        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-          <div class="resume-content">
-            <h3 class="mb-0">Teacher of Science</h3>
-            <div class="subheading mb-3">Northampton Academy</div>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-          </div>
-          <div class="resume-date text-md-right">
-            <span class="text-primary">September 2014 - August 2016</span>
-          </div>
-        </div>
+            if(get_field('end_date')){
+              $endDate = new DateTime(get_field('end_date'));
+              $formattedEndDate = $endDate->format('F').' '.$endDate->format('Y');
+            }
+            ?>
+
+            <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+              <div class="resume-content">
+                <h3 class="mb-0"><?php echo the_title()?></h3>
+                <div class="subheading mb-3"><?php echo the_field('institution')?></div>
+                <p><?php echo the_content() ?></p>
+              </div>
+              <div class="resume-date text-md-right">
+                <?php $startDate = new DateTime(get_field('start_date')); ?>
+                <span class="text-primary"><?php echo $startDate->format('F').' '.$startDate->format('Y')?> - <?php echo $formattedEndDate ?></span>
+              </div>
+            </div>
+
+          <?php } ?>
 
       </div>
 
