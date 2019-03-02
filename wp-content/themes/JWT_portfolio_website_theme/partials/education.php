cdec2f600e55e3e615894b08ff43c8a3de49bdde
jamie wright <https://jamieianwright@bitbucket.org/jamieianwright/>
2019-03-02 10:05:43 +0000

    Education now populated dynamically by wp rather than static HTML

--- wp-content/themes/JWT_portfolio_website_theme/partials/education.php
+++ wp-content/themes/JWT_portfolio_website_theme/partials/education.php
@@ -2,40 +2,36 @@
     <div class="w-100">
     <h2 class="mb-5">Education</h2>
 
-    <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-        <div class="resume-content">
-        <h3 class="mb-0">University of Nottingham</h3>
-        <div class="subheading mb-3">Post Graduate Certificate of Secondary Education - Science</div>
-        <p>Rating: Good</p>
-        </div>
-        <div class="resume-date text-md-right">
-        <span class="text-primary">July 2014 - August 2015</span>
-        </div>
-    </div>
+    <?php 
+          $schools = new WP_Query(array(
+            'post_type' => 'education',
+            'orderby' => 'start_date',
+            'order' => 'ASC',
+          ));
 
-    <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-        <div class="resume-content">
-        <h3 class="mb-0">University of Stirling</h3>
-        <div class="subheading mb-3">Bachelor of Science - Cell Biology</div>
-        <p>First Class</p>
-        </div>
-        <div class="resume-date text-md-right">
-        <span class="text-primary">September 2009 - June 2013</span>
-        </div>
-    </div>
+          while($schools->have_posts()) {
+            $schools->the_post();
+            $formattedEndDate = 'Present';
 
-    <div class="resume-item d-flex flex-column flex-md-row justify-content-between">
-        <div class="resume-content">
-        <h3 class="mb-0">Allestree Woodland School</h3>
-        <div class="subheading mb-3">A-levels</div>
-        <p>Biology: B</p>
-        <p>Mathematics: C</p>
-        <p>Physics: C</p>
-        </div>
-        <div class="resume-date text-md-right">
-        <span class="text-primary">September 2002 - June 2008</span>
-        </div>
-    </div>
+            if(get_field('end_date')){
+              $endDate = new DateTime(get_field('end_date'));
+              $formattedEndDate = $endDate->format('F').' '.$endDate->format('Y');
+            }
+            ?>
+
+            <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="resume-content">
+                <h3 class="mb-0"><?php echo the_title() ?></h3>
+                <div class="subheading mb-3"><?php echo the_field('course')?></div>
+                <p><?php echo the_field('grades')?></p>
+                </div>
+                <div class="resume-date text-md-right">
+                    <?php $startDate = new DateTime(get_field('start_date')); ?>
+                    <span class="text-primary"><?php echo $startDate->format('F').' '.$startDate->format('Y')?> - <?php echo $formattedEndDate ?></span>
+                </div>
+            </div>
+
+          <?php } ?>
 
     </div>
 </section>
